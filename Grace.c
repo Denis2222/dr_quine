e668fa75ce3b5f1d8d2ea7315fbbf400ba9a811e
Denis MOUREU-LARRANG <dmoureu-@e1r10p6.42.fr>
2017-03-27 22:43:33 +0200

    OK

---
+++ Grace.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+/*
+	OUT
+*/
+#define MAIN int main(){brainfuck();}
+void brainfuck(){char*a="#include<stdio.h>%c%c%c%c	OUT%c%c%c%c#define MAIN int main(){brainfuck();}%cvoid brainfuck(){char*a=%c%s%c;FILE *f = fopen(%cGrace_kid.c%c,%cw%c);fprintf(f,a,10,47,42,10,10,42,47,10,10,34,a,34,34,34,34,34,10);};MAIN;%c";FILE *f = fopen("Grace_kid.c","w");fprintf(f,a,10,47,42,10,10,42,47,10,10,34,a,34,34,34,34,34,10);};MAIN;
