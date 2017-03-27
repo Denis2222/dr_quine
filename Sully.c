8ffda38cd301a2d965414f4e67a324352802724f
Denis MOUREU-LARRANG <dmoureu-@e1r10p6.42.fr>
2017-03-28 00:15:18 +0200

    Presque

--- Sully.c
+++ Sully.c
@@ -1,20 +1,15 @@
+#include<stdlib.h>
 #include<stdio.h>
+#define EXEC "./Sully_%d"
+#define GCC "gcc -Wall -Wextra -Wall -o Sully_%d Sully_%d.c" 
+#define FILENAME "Sully_%d.c"   
+#define WMODE "w"
 int main(void){
-
 int X = 5;
 char *file;
-asprintf(&file, "Sully_%d.c", X);
-
-char*a="#include<stdio.h>%d";
-
-printf("%s\n", file);
-FILE *f = fopen(file,"w");
-fprintf(f,a,X-1);
-
-if (X>0)
-{
-	char *line;
-	asprintf(&line, "gcc -Wall -Wextra -Wall -o Sully_%d Sully_%d.c && ./Sully_%d", X,X,X);
-	system(line);
-}
-};
+asprintf(&file, FILENAME, X);
+char*a="#include<stdlib.h>%c#include<stdio.h>%c#define EXEC %c%s%c%c#define GCC %c%s%c%c#define FILENAME %c%s%c%c#define WMODE %c%s%c%cint main(void){int X = %d;char *file;asprintf(&file, FILENAME, X);char*a=%c%s%c;FILE *f = fopen(file,WMODE);fprintf(f,a,10,10,34,EXEC,34,10,34,GCC,34,10,34,FILENAME,34,10,34,WMODE,34,10, X-1, 34,a,34);fclose(f);if (X>0){char *line;char *file;asprintf(&file, EXEC, X);asprintf(&line, GCC, X,X);system(line);system(file);}};";
+FILE *f = fopen(file,WMODE);
+fprintf(f,a,10,10,34,EXEC,34,10,34,GCC,34,10,34,FILENAME,34,10,34,WMODE,34,10, X-1, 34,a,34);
+fclose(f);
+if (X>0){char *line;char *file;asprintf(&file, EXEC, X);asprintf(&line, GCC, X,X);printf("%s\n", line);system(line);system(file);}};
