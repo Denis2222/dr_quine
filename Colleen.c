923dada5071959ba099ee8efce46fa578a04fef2
Denis MOUREU-LARRANG <dmoureu-@e1r10p6.42.fr>
2017-03-27 21:04:55 +0200

    Colleen

---
+++ Colleen.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/*
+	OUT
+*/
+void brainfuck(){};int main(){
+/*
+	IN
+*/
+char*a="#include<stdio.h>%c%c%c%c	OUT%c%c%c%cvoid brainfuck(){};int main(){%c%c%c%c	IN%c%c%c%cchar*a=%c%s%c;printf(a,10,47,42,10,10,42,47,10,10,47,42,10,10,42,47,10,34,a,34,10);}%c";printf(a,10,47,42,10,10,42,47,10,10,47,42,10,10,42,47,10,34,a,34,10);}
