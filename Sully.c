1de6493627424e8a73ed623fab7fbaf7900c5bba
Denis MOUREU-LARRANG <dmoureu-@e1r10p6.42.fr>
2017-03-28 19:28:43 +0200

    OK

--- Sully.c
+++ Sully.c
@@ -1,9 +1,12 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 #define EXEC "./Sully_%d"
 #define GCC "gcc -Wall -Wextra -Wall -o Sully_%d Sully_%d.c"
 #define FILENAME "Sully_%d.c"
 #define WMODE "w"
+#define PRINT "#include<stdlib.h>%c#include<stdio.h>%c#include<string.h>%c#define EXEC %c%s%c%c#define GCC %c%s%c%c#define FILENAME %c%s%c%c#define WMODE %c%s%c%c#define PRINT %c%s%c%c#define SOURCE %c%s%c%cint main(void){%cint X = %d;%cchar*line;char*file;if(strcmp(__FILE__,SOURCE))X--;asprintf(&file, FILENAME, X);FILE *f = fopen(file,WMODE);fprintf(f,PRINT,10,10,10,34,EXEC,34,10,34,GCC,34,10,34,FILENAME,34,10,34,WMODE,34,10,34,PRINT,34,10,34,SOURCE,34,10,10,X,10,10);fclose(f);asprintf(&file, EXEC, X);asprintf(&line, GCC, X,X);system(line);if(X>0){system(file);}};%c"
+#define SOURCE "../Sully.c"
 int main(void){
 int X = 5;
-char *file;asprintf(&file, FILENAME, X);char*a="#include<stdlib.h>%c#include<stdio.h>%c#define EXEC %c%s%c%c#define GCC %c%s%c%c#define FILENAME %c%s%c%c#define WMODE %c%s%c%cint main(void){%cint X = %d;%cchar *file;asprintf(&file, FILENAME, X);char*a=%c%s%c;FILE *f = fopen(file,WMODE);fprintf(f,a,10,10,34,EXEC,34,10,34,GCC,34,10,34,FILENAME,34,10,34,WMODE,34,10,10,X-1,10, 34,a,34);fclose(f);if (X>0){char *line;char *file;asprintf(&file, EXEC, X);asprintf(&line, GCC, X,X);system(line);system(file);}};";FILE *f = fopen(file,WMODE);fprintf(f,a,10,10,34,EXEC,34,10,34,GCC,34,10,34,FILENAME,34,10,34,WMODE,34,10,10,X-1,10,34,a,34);fclose(f);if (X>0){char *line;char *file;asprintf(&file, EXEC, X);asprintf(&line, GCC, X,X);printf("%s\n", line);system(line);system(file);}};
+char*line;char*file;if(strcmp(__FILE__,SOURCE))X--;asprintf(&file, FILENAME, X);FILE *f = fopen(file,WMODE);fprintf(f,PRINT,10,10,10,34,EXEC,34,10,34,GCC,34,10,34,FILENAME,34,10,34,WMODE,34,10,34,PRINT,34,10,34,SOURCE,34,10,10,X,10,10);fclose(f);asprintf(&file, EXEC, X);asprintf(&line, GCC, X,X);system(line);if(X>0){system(file);}};
