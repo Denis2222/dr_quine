3762a47d3b29e3e323f55e96f6e0fbb1fac7b16e
Denis MOUREU-LARRANG <dmoureu-@e1r10p6.42.fr>
2017-03-28 11:37:09 +0200

    Update Grace 3MACRO

--- Grace.c
+++ Grace.c
@@ -2,5 +2,7 @@
 /*
 	OUT
 */
-#define MAIN int main(){brainfuck();}
-void brainfuck(){char*a="#include<stdio.h>%c%c%c%c	OUT%c%c%c%c#define MAIN int main(){brainfuck();}%cvoid brainfuck(){char*a=%c%s%c;FILE *f = fopen(%cGrace_kid.c%c,%cw%c);fprintf(f,a,10,47,42,10,10,42,47,10,10,34,a,34,34,34,34,34,10);};MAIN;%c";FILE *f = fopen("Grace_kid.c","w");fprintf(f,a,10,47,42,10,10,42,47,10,10,34,a,34,34,34,34,34,10);};MAIN;
+#define MAIN int main(){brainfuck();return(0);}
+#define FILENAME "Grace_kid.c"
+#define WMODE "w"
+void brainfuck(){char*a="#include<stdio.h>%c%c%c%c	OUT%c%c%c%c#define MAIN int main(){brainfuck();return(0);}%c#define FILENAME %c%s%c%c#define WMODE %c%s%c%cvoid brainfuck(){char*a=%c%s%c;FILE *f = fopen(FILENAME,WMODE);fprintf(f,a,10,47,42,10,10,42,47,10,10,34,FILENAME,34,10,34,WMODE,34,10,34,a,34,10);};MAIN;%c";FILE *f = fopen(FILENAME,WMODE);fprintf(f,a,10,47,42,10,10,42,47,10,10,34,FILENAME,34,10,34,WMODE,34,10,34,a,34,10);};MAIN;
