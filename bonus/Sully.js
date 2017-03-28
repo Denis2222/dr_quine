b98590ced9925165bf596c064a77971a61358f63
Denis MOUREU-LARRANG <dmoureu-@e1r10p6.42.fr>
2017-03-28 20:25:10 +0200

    auteur + bonus

--- bonus/Sully.js
+++ bonus/Sully.js
@@ -2,23 +2,14 @@ i = 5;
 (function $(){
 	path = require('path');
 	var filename = path.basename(__filename);
-
-function strcmp(a, b) {
-    if (a.toString() < b.toString()) return -1;
-    if (a.toString() > b.toString()) return 1;
-    return 0;
-}
-
-	//console.log("Yolo?",filename.toString() ,filename.toString() === "Sully.c");
-
-	//console.log(strcmp(filename, "Sully._js"));
-
-	console.log("Comparaison : strcmp(filename, Sully._js)", strcmp(filename, "Sully.js"));
-
+	function strcmp(a, b) {
+	    if (a.toString() < b.toString()) return -1;
+	    if (a.toString() > b.toString()) return 1;
+	    return 0;
+	}
 	if ((strcmp(filename, "Sully.js")))
 		i--;
 	var fs = require('fs');
-
 	fs.writeFile("./Sully_"+i+".js","i = "+i+";\n("+$.toString()+")();\n" , function(err) {
 	    if(err) {
 	        return console.log(err);
